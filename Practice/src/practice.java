3bc7612e49f8b9eb9b5c11239f36e920679efb68
takutaku <takuminmin427@gmail.com>
2024-10-16 00:07:08 +0900

    practice.java 変更

--- Practice/src/practice.java
+++ Practice/src/practice.java
@@ -4,7 +4,7 @@ public class practice {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("Hello world!");
-		System.out.println("どうしたらいいの～～～～～～");
+		System.out.println("どうしたらいいの～～～～～～～～～～");
 		int i = 0;
 		System.out.println(i);
 	}
