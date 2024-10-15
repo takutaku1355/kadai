a392e4af12d845ea8584bb980e6df909ded6be14
takutaku <takuminmin427@gmail.com>
2024-10-16 00:06:17 +0900

    takahashi.java 変更

--- Practice/src/takahashi.java
+++ Practice/src/takahashi.java
@@ -4,6 +4,11 @@ public class takahashi {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("私はたくみん");
+		int num = 0;
+		for (int i = 0; i < 5; i++) {
+			num++;
+			System.out.println(num);
+		}
 
 	}
 
